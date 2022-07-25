fa9fe1183fb04030720dc0c72271d1f3d3e0c452
rajeshpatnala <rajeshpatnala96@gmail.com>
2022-07-25 11:54:13 +0530

    uploading constants.py script

---
+++ constants.py
@@ -0,0 +1,23 @@
+INPUT_DATASETS_DIR = "input_files"
+
+TEST_DATASETS_DIR = "test_files/input_data/"
+
+TEST_RAW_DATASETS_DIR = "test_files/input_data/raw_files"
+
+TEST_EXPECTED_DATASETS_DIR = "test_files/expected_files"
+
+TEST_DATASET_CONFIG = [{
+    "FILE_NAME": "IND.csv",
+    "FILE_ROWS": 3,
+    "COUNTRY": "IND"
+}, {
+    "FILE_NAME": "USA.csv",
+    "FILE_ROWS": 3,
+    "COUNTRY": "USA"
+}, {
+    "FILE_NAME": "AUS.xlsx",
+    "FILE_ROWS": 3,
+    "COUNTRY": "AUS"
+}]
+
+COUNTRY_POPULATION = {"IND": 8, "USA": 6, "AUS": 5}
